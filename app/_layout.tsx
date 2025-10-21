ab46a8b03bb0c69182233f1d3009ad0744184e1b
Abdulrahman Nasser <abdulrahman.nasser222@gmail.com>
2025-10-21 11:00:56 +0300

    Improve the header styling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,20 +6,19 @@ import NetflixHeader from './components/NetflixHeader';
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: { backgroundColor: '#111' },
-        headerTitle: () => <NetflixHeader />, // Custom logo
-        headerTitleAlign: 'center',
-        headerShadowVisible: false,
-        headerBackVisible: false,
-        animation: 'slide_from_right',
-      }}
-    >
-      <Stack.Screen name="index" options={{ title: 'Home' }} />
-      <Stack.Screen name="HomeScreen" options={{ title: 'Home' }} />
-      <Stack.Screen name="LoginScreen" options={{ title: 'Login' }} />
-      <Stack.Screen name="RegisterScreen" options={{ title: 'Register' }} />
-    </Stack>
+    <>
+      <NetflixHeader />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          animation: 'slide_from_right',
+        }}
+      >
+        <Stack.Screen name="index" options={{ title: 'Home' }} />
+        <Stack.Screen name="HomeScreen" options={{ title: 'Home' }} />
+        <Stack.Screen name="LoginScreen" options={{ title: 'Login' }} />
+        <Stack.Screen name="RegisterScreen" options={{ title: 'Register' }} />
+      </Stack>
+    </>
   );
 }
--- app/components/NetflixHeader.tsx
+++ app/components/NetflixHeader.tsx
@@ -5,16 +5,16 @@ import { View } from 'react-native';
 const NetflixHeader: React.FC = () => {
     
   return (
-    <View className="flex-row items-center justify-center w-full px-4 h-14">
+    <View className="absolute top-0 left-0 right-0 flex-row items-center justify-between px-4 h-14 bg-transparent z-10">
       <Link
-      href={'/'} className='text-red-600 mr-20 font-bold text-lg mr-4'>Netflix</Link>
+      href={'/'} className='text-red-600 font-bold text-lg'>Netflix</Link>
 
       <View className="flex-row space-x-2">
         <Link
-      href={'/LoginScreen'} className=' text-white bg-red-600 py-1.5 px-3 rounded   ml-8'>Login</Link>
+      href={'/LoginScreen'} className=' text-white bg-red-600 py-1.5 px-3 rounded'>Login</Link>
 
         <Link
-      href={'/RegisterScreen'} className='text-white py-1.5 px-3 rounded   '>Sign Up</Link>
+      href={'/RegisterScreen'} className='text-white py-1.5 px-3 rounded'>Sign Up</Link>
 
       </View>
     </View>
